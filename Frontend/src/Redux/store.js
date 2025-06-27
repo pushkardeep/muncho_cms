00f9b1fa1734796729d09ec723a8151d2a9a3237
kalpit-muncho <kalpit@muncho.in>
2025-06-27 15:20:18 +0530

    delete

--- Frontend/src/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-// Slices
-import sectionSlice from "./Slices/sections.slice";
-export const store = configureStore({
-  reducer: {
-    sections: sectionSlice,
-  },
-});
